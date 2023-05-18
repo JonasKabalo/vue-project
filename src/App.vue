e635e7e1419390c89509f21e058a0340a4779c9d
jonaskabalo <kabalojonas44@gmail.com>
2023-05-18 18:02:32 +0100

    update dom clean old code

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@ defineComponent({
   }
 })
 
-import HelloWorld from './components/HelloWorld.vue'
 import useEventsBus from './plugins/eventBus'
 const { busOther } = useEventsBus()
 
@@ -83,11 +82,7 @@ export default {
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -138,7 +133,7 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
@@ -163,5 +158,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
